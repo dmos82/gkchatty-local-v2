6bffb1525f28515e680399297777dd474c785599
David <davidmorinmusic@gmail.com>
2025-11-08 15:03:59 -0700

    fix: Add @ts-ignore for Promise.withResolvers polyfill check

--- packages/web/src/lib/polyfills.ts
+++ packages/web/src/lib/polyfills.ts
@@ -1,5 +1,6 @@
 // Polyfill for Promise.withResolvers() - required for Node.js < 22
 // This is needed for pdfjs-dist@4.8.69 to work on Node.js v20
+// @ts-ignore - withResolvers not in standard TypeScript lib yet
 if (typeof Promise.withResolvers === 'undefined') {
   // @ts-expect-error - Adding polyfill to Promise prototype
   Promise.withResolvers = function <T>() {
